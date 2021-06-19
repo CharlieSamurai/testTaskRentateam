9292fb41385b8232e466a593c04ff6009891776f
Georgii <animehowdy@gmail.com>
2021-06-19 04:31:55 +0300

    some improves

--- client/src/Redux/ReduxThunks/getCurrentCategory.js
+++ client/src/Redux/ReduxThunks/getCurrentCategory.js
@@ -7,8 +7,9 @@ const getCurrentCategory = (categoryObj) => {
 
 const getCurrentCategoryThunk = (categoryName) => async (dispatch) => {
   const response = await fetch(`http://localhost:3001/getcurrentcategory/${categoryName}`);
-  const serverResponse = await response.json();
-  dispatch(getCurrentCategory(serverResponse));
+  if (response.status === 200) {
+    dispatch(getCurrentCategory(await response.json()));
+  };
 };
 
 export default getCurrentCategoryThunk;
